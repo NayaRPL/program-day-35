50ef73ff346f87cd6efad4c48456a7956c64976d
NayaRPL <naya.mmj@gmail.com>
2022-01-15 21:20:40 +0800

    day 35

---
+++ program day 35.py
@@ -0,0 +1,30 @@
+data1=["naya",45,False,True,"awal"]
+data2=("naya",45,False,True,"awal")
+print("tipe data list:",data1)
+print("tipe data tuple:",data2)
+
+
+
+nama=["majene","pinrang","palu","pasangkayu","makassar","polewali","pare","campa"]
+print(nama)
+nama.reverse()
+print(nama)
+
+angka=[11,12,13,14,15,16,17,]
+print(angka[4:])
+angka.insert(3, 20)
+print(angka)
+
+print("orang")
+mat=[[1,2,3],
+     [4,5,7],
+     [5,6,8]]
+colum=[]
+for sin in mat:
+    colum.append(sin[1])
+    print(colum)
+
+
+
+
+
